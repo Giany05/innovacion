c3ebcd1c49b9463d56aff645f24f514cfa86e18e
Giany05 <130597348+Giany05@users.noreply.github.com>
2023-09-21 23:55:59 -0500

    Add files via upload

---
+++ guardar.php
@@ -0,0 +1,25 @@
+<?php
+require 'conexion.php';
+
+$email = $_POST['email'];
+
+$insertar = "INSERT INTO clientes VALUES ('email')";
+
+$query = mysqli_query($conectar, $insertar);
+  
+  if(query) {
+    echo "<script> alert('correcto');
+    location.href = '../index.html'
+     </script>";
+
+  }else{
+    echo "<script> alert('incorrecto'); 
+    location.href = '../index.html'
+    </script>";
+  }
+
+  
+
+
+
+?>
